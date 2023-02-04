81716edbfe84061086dc62063ab8a016a741a953
G Gautham Krishna <ggauthamkrishna@gmail.com>
2023-02-04 10:58:27 +0530

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,18 @@
-
-
-  module.exports = ({ env }) => ({
-    // ...
-   
-    // ...
-  });
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'dfbuus9y7',
+        api_key: '297491442381676',
+        api_secret: 'B-NZj5qj_xRoNNDSrAhsHAcQdrU',
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
