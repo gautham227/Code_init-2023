fcbba3b2a2c290db78bd4a4761653dfd71b6a2d4
Sidharth Deepesh <sidharthdeepesh@hotmail.com>
2023-02-04 12:17:19 +0530

    modularized ml search

--- ML Models/similarsearch.py
+++ ML Models/similarsearch.py
@@ -1,11 +1,4 @@
 from sentence_transformers import SentenceTransformer, util
-sentences = ['Umbrella found which is like a newspaper',
-              'Pasta found',
-             'Found phone samsung',
-             'Found laptop',
-             'Found TV',
-             'Found realme phone',
-             'Found gautham near prateeksha']
 model = SentenceTransformer('all-MiniLM-L6-v2')
 def search(inputtext,database):
     sentences=inputtext+database
@@ -20,4 +13,3 @@ def search(inputtext,database):
         i, j = pair['index']
         print("{} \t\t {} \t\t Score: {:.4f}".format(sentences[i], sentences[j], pair['score']))
     return pairs
-print(search(['Found an Umbrella like a newspaper'],sentences))
